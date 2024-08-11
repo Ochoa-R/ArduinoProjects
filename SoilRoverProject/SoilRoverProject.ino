835f1fc1b3af4b018d89b2d76df0110e9792336c
R Ochoa <167141768+Ochoa-R@users.noreply.github.com>
2024-08-10 18:00:46 -0700

    Update SoilRoverProject.ino

--- SoilRoverProject/SoilRoverProject.ino
+++ SoilRoverProject/SoilRoverProject.ino
@@ -1,16 +1,33 @@
+/*Reads temperature and humidity every 2 seconds while a servo motor sweeps back and forth
+  if an object gets too close to the ultrasonic ping sensor, the motor ceases sweeping
+  once the object leaves the set distance, motor returns sweeping. proof of concept for
+  the motors on the chassis of the rover
+  
+  TO DO: Figure out how to pulse the Ultrasonic Sensor asychronously, otherwise it messes
+  with timing of the motor and temp/hum sensor*/
+
+
 #include <Adafruit_BME680.h>
+#include <NewServo.h>
+#include "roverVaribles.h"
 
 Adafruit_BME680 bmeSensor;
-
-namespace Timing
-{
-  volatile unsigned long lastRead = millis();
-  const unsigned long readInterval = 2000;
-}
+NewServo servMotor(Pins::motor);
 
 void setup() 
 {
+  using namespace Pins;
   Serial.begin(9600);
+  while(!Serial)
+    ;
+
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+
+  servMotor.setMax(170);
+  servMotor.setMin(10);
+  servMotor.setInit(90);
+
   bmeSensor.begin();
   if(!bmeSensor.begin())
   {
@@ -23,31 +40,77 @@ void setup()
   bmeSensor.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bmeSensor.setGasHeater(320, 150);
 
+  servMotor.begin();
+  servMotor.goInit();
   Serial.println("All good!");
 }
 
 void loop() 
 {
+  //static bool safeDistance;
   using namespace Timing;
+
+  if((millis() - lastMove > motorInterval))// && safeDistance == true)
+  {
+    moveMotor();
+    lastMove = millis();
+  }
+
+  if(bmeSensor.beginReading() == 0)
+  {
+    Serial.println("reading failed to begin.");
+    return;
+  }
+
   if(millis() - lastRead > readInterval)
   {
-    if(!bmeSensor.performReading())
+    if(!bmeSensor.endReading())
     {
-      Serial.println("reading failed.");
+      Serial.println("reading failed to end.");
       return;
     } 
     printReading();
     lastRead = millis();
   }
+
+  /*if(millis() - lastPulse > pulseInterval)
+  {
+    unsigned int distanceCm = pulseRead() / 29 / 2;
+    (distanceCm < 15) ? (safeDistance = false) : (safeDistance = true);
+  }*/
 }
 
 void printReading()
 {
   Serial.print("Temperature: ");
-  Serial.print(bmeSensor.temperature);
-  Serial.println(" *C");
+  Serial.print(bmeSensor.temperature * 1.8 + 32);
+  Serial.println(" *F");
 
   Serial.print("Humidity: ");
   Serial.print(bmeSensor.humidity);
   Serial.println(" %");
+}
+
+void moveMotor()
+{
+  static bool sweepFlag = true; 
+  static byte motorPos = 90;
+
+  if(motorPos >= 170)
+    sweepFlag = false;
+  if(motorPos <= 10)
+    sweepFlag = true;
+  
+  sweepFlag ? (++motorPos) : (--motorPos);
+  
+  servMotor.move(motorPos);
+}
+
+unsigned long pulseRead()
+{
+  digitalWrite(Pins::trigger, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(Pins::trigger, LOW);
+
+  return pulseIn(Pins::echo, HIGH);
 }
