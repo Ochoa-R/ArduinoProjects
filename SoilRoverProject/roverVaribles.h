00f7591c3e2f7c5043bc708f92f01990da20eb77
R Ochoa <167141768+Ochoa-R@users.noreply.github.com>
2024-08-19 20:27:55 -0700

    Update roverVaribles.h

--- SoilRoverProject/roverVaribles.h
+++ SoilRoverProject/roverVaribles.h
@@ -1,13 +1,12 @@
+#ifndef ROVER_VAR_H
+#define ROVER_VAR_H
+
 namespace Timing
 {
-  volatile unsigned long lastRead = 0; 
   volatile unsigned long lastMove = 0;
   volatile unsigned long lastPulse = 0;
-  volatile unsigned long lastWater = 0;
-  const unsigned int readInterval = 2000;
   const byte pulseInterval = 200;
   const byte motorInterval = 10;
-  const unsigned int waterInterval = 2000;
 }
 
 namespace Pins
@@ -17,4 +16,19 @@ const byte trigger = A1;
 const byte echo = A2;
 const byte led = A6;
 const byte motor = 6;
+const byte toggle = 9;
+const byte RE = 8;
+const byte DE = 7;
+const byte RO = 10;
+const byte DI = 11; 
 }
+
+void moving();
+void stopped();
+void moveMotor();
+void pulseCheck();
+void checkNPK();
+void printReading(unsigned int& water);
+unsigned int checkWater();
+
+#endif
